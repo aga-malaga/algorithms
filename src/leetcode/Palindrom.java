7d289df864645e4bda03baa65796bb0701b26f2e
Agnieszka Król <mme.nowobilska@gmail.com>
2022-12-26 22:20:30 +0100

    implement valid palindrom

---
+++ src/leetcode/Palindrom.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+class Palindrom {
+    public static void main(String[] args) {
+        String s = "race a car";
+        String s2 = "A man, a plan, a canal: Panama";
+        String s3 = " ";
+        String s4 = "ab_a";
+//        System.out.println(isPalindrome(s));
+//        System.out.println(isPalindrome(s2));
+//        System.out.println(isPalindrome(s3));
+        System.out.println(isPalindrome(s4));
+    }
+
+    public static boolean isPalindrome(String s) {
+        if (s.equals(" ")) {
+            return true;
+        }
+        String result = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+        int j = result.length() - 1;
+        for (int i = 0; i < result.length(); i++) {
+                if (result.charAt(i) != result.charAt(j) && j >= i) {
+                    return false;
+                }
+                j--;
+        }
+
+        return true;
+    }
+
+}
--- src/leetcode/RotateArray.java
+++ src/leetcode/RotateArray.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 
 class RotateArray {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(rotate(new int[]{1,2,3,4,5,6,7}, 3)));
-        System.out.println(Arrays.toString(rotate2(new int[]{1,2,3,4,5,6,7}, 3)));
-        System.out.println(Arrays.toString(rotate(new int[]{1,2,3,4,5}, 3)));
-        System.out.println(Arrays.toString(rotate2(new int[]{1,2,3,4,5}, 3)));
-        System.out.println(Arrays.toString(rotate(new int[]{1,2,3,4,5,6,7}, 3)));
-        System.out.println(Arrays.toString(rotate2(new int[]{1,2,3,4,5,6,7}, 3)));
-        System.out.println(Arrays.toString(rotate(new int[]{1,2,3,4,5,6,7,8,9,10}, 3)));
-        System.out.println(Arrays.toString(rotate2(new int[]{1,2,3,4,5,6,7,8,9,10}, 3)));
+        System.out.println(Arrays.toString(rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3)));
+        System.out.println((rotate3(new int[]{1, 2, 3, 4, 5, 6, 7}, 3)));
+        System.out.println(Arrays.toString(rotate(new int[]{1, 2, 3, 4, 5}, 3)));
+        System.out.println((rotate3(new int[]{1, 2, 3, 4, 5}, 3)));
+        System.out.println(Arrays.toString(rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3)));
+        System.out.println((rotate3(new int[]{1, 2, 3, 4, 5, 6, 7}, 3)));
+        System.out.println(Arrays.toString(rotate(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3)));
+        System.out.println((rotate3(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3)));
     }
 
-    public static int[] rotate(int[] nums, int k){
-       int[] result = new int[nums.length];
+    public static int[] rotate(int[] nums, int k) {
+        int[] result = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            if ((i + k) < nums.length ) {
+            if ((i + k) < nums.length) {
                 result[i] = nums[i + k];
             } else {
                 result[i] = nums[(i + k) - nums.length];
@@ -26,15 +26,17 @@ class RotateArray {
         return result;
     }
 
-
-
+    public static int rotate3(int[] nums, int k) {
+        System.out.println("nums.length: " + nums.length + ", k: " + k);
+        return nums.length % k;
+    }
 
     public static int[] rotate2(int[] nums, int k) {
         int[] result = new int[nums.length];
         k = k % nums.length;
         int rotate = nums.length - k;
         for (int i = 0; i < nums.length; i++) {
-            if (rotate >= nums.length){
+            if (rotate >= nums.length) {
                 rotate = rotate - nums.length;
             }
             result[i] = nums[rotate];
